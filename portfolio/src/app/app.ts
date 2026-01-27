9532fd3121e37ccf96ee75f57305870553de94a7
pedro <pedrokawa10@outlook.com>
2026-01-27 13:31:05 -0300

    first commit

---
+++ portfolio/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
+import { Home } from "./components/home/home";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, Footer, Home],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('portfolio');
+}
