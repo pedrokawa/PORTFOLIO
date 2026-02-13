3c3911a9fc98d59e631689eff3a9fb2928aee9ce
pedro <pedrokawa10@outlook.com>
2026-02-13 15:23:35 -0300

    fix: fix backend url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-portfolio-36kuowcn4-tecnologia-asfaltopavs-projects.vercel.app/' 
+  apiUrl: 'https://backend-portfolio-36kuowcn4-tecnologia-asfaltopavs-projects.vercel.app/api' 
 };
